0537ff25d0704b7c87aa359ea9f32b6b975c0d0a
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-08-29 15:06:41 -0400

    Add useState
    - Add useState to add First Name and Last Name to greeting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fname, setName1] = useState("");
+  const [headingText1, setHeading1] = useState("");
+
+  const [lname, setName2] = useState("");
+  const [headingText2, setHeading2] = useState("");
+
+  function handleChange(event) {
+    setName1(event.target.value);
+  }
+
+  function handleChange2(event) {
+    setName2(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading1(fname);
+    setHeading2(lname);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {headingText1} {headingText2}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="fName"
+          placeholder="First Name"
+          value={fname}
+        />
+        <input
+          onChange={handleChange2}
+          type="text"
+          name="lName"
+          placeholder="Last Name"
+          value={lname}
+        />
+        <button onClick={handleClick}>Submit</button>
       </form>
     </div>
   );
